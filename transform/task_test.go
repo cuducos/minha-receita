157f1114c87526f12fdbbb91d4a1f650e4b17686
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-12-06 14:10:32 -0500

    Creates command to create 1 JSON file per CNPJ
    Co-authored-by: danielfireman-ifal <daniel.fireman@ifal.edu.br>

---
+++ transform/task_test.go
@@ -0,0 +1,60 @@
+package transform
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestTaskRun(t *testing.T) {
+	d := t.TempDir()
+	ls, err := PathsForSource(venue, filepath.Join("..", "testdata"))
+	if err != nil {
+		t.Errorf("expected no error finding paths for %s, got %s", string(venue), err)
+	}
+	for _, f := range ls {
+		copyFile(f, d)
+	}
+	p, err := newTask(d, venue)
+	if err != nil {
+		t.Errorf("expected no error creating task, got %s", err)
+	}
+	if err = p.run(2); err != nil {
+		t.Errorf("expected no error running task, got %s", err)
+	}
+
+	var j []string
+	err = filepath.WalkDir(d, func(p string, d os.DirEntry, err error) error {
+		if strings.HasSuffix(p, ".json") {
+			j = append(j, p)
+		}
+		return nil
+	})
+	if err != nil {
+		t.Errorf("expected no error walking %s, got %s", d, err)
+	}
+	if len(j) != 1 {
+		t.Errorf("expected 1 JSON files, got %d", len(j))
+	}
+}
+
+func copyFile(src string, targetDir string) error {
+	s, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("expected no error opening file %s, got %s", src, err)
+	}
+	defer s.Close()
+	o := filepath.Join(targetDir, filepath.Base(src))
+	t, err := os.Create(o)
+	if err != nil {
+		return fmt.Errorf("expected no error creating file %s, got %s", o, err)
+	}
+	defer t.Close()
+	if _, err := io.Copy(t, s); err != nil {
+		return fmt.Errorf("expected no error copying file %s to %s, got %s", src, o, err)
+	}
+	return nil
+}
