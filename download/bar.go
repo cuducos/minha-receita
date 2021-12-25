c172b98eb5e87951651d92473a3148ac9a9fab1c
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-12-25 12:19:51 -0500

    Fixes file count at the end of the download progress bar

--- download/bar.go
+++ download/bar.go
@@ -17,10 +17,6 @@ type downloadProgressBar struct {
 	updateTotal chan struct{}
 }
 
-func (b *downloadProgressBar) isFinished() bool {
-	return b.total == b.done
-}
-
 func (b *downloadProgressBar) Write(d []byte) (int, error) {
 	b.updateBytes <- int64(len(d))
 	return len(d), nil
@@ -28,4 +24,25 @@ func (b *downloadProgressBar) Write(d []byte) (int, error) {
 
 func (b *downloadProgressBar) description() string {
 	return fmt.Sprintf("Downloading (%d of %d files done)", b.done, b.total)
+}
+
+func (b *downloadProgressBar) update() {
+	b.main.Describe(b.description())
+}
+
+func (b *downloadProgressBar) addBytes(n int64) {
+	// on the last file, the file count must be updated before adding bytes,
+	// otherwise the description won't update
+	if b.done == b.total-1 {
+		if b.main.State().CurrentBytes+float64(n) == float64(b.main.GetMax64()) {
+			b.done = b.total
+			b.update()
+		}
+	}
+	b.main.Add64(n)
+}
+
+func (b *downloadProgressBar) addFile() {
+	b.done++
+	b.update()
 }
