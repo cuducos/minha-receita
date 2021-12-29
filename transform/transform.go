c0b5ca7704e450eaccf9f21e3cc1adaf922b8a0e
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-12-28 21:02:43 -0500

    Renames: task.go -> venues.go

--- transform/transform.go
+++ transform/transform.go
@@ -8,7 +8,7 @@ const maxFilesOpened = 512 // TODO how to optimize this number?
 
 // Transform the downloaded files for company venues creating a JSON file per CNPJ
 func Transform(srcDir, outDir string) error {
-	t, err := newTask(srcDir, outDir)
+	t, err := createJSONFiles(srcDir, outDir)
 	if err != nil {
 		return fmt.Errorf("error creating new task for venues in %s: %w", srcDir, err)
 	}
