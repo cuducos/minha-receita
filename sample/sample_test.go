8532bebdcc8b65426db53264c5701303d73059af
Alexandre Castro <im-alexandre07@gmail.com>
2022-04-01 13:51:09 -0300

    Refatorando sample_test
    - [x] Substituindo "testdata/sample"
    - [x] Checando erros
    - [x] Internalizando variável fileExt
    - [x] Deslocando instrução para fora do loop

--- sample/sample_test.go
+++ sample/sample_test.go
@@ -9,27 +9,31 @@ import (
 )
 
 func TestSample(t *testing.T) {
-    src, dst := "../testdata", "../testdata/sample"
-    Sample(src, dst, 100)
+    src, dst := "../testdata", t.TempDir()
+    if err:= Sample(src, dst, 100); err != nil {
+        t.Fatalf("expected no error running sample, got %s", err)
+    }
     dataFiles := listFilesFromDir(t, src)
     sampleFiles := listFilesFromDir(t, dst)
-    if ! reflect.DeepEqual(dataFiles, sampleFiles) {
+    if !reflect.DeepEqual(dataFiles, sampleFiles) {
         t.Errorf("File not found in sample")
     }
 }
 
 func listFilesFromDir(t *testing.T, dir string) []string {
+    ext := []string{"json", "csv", "zip"}
     var files []string
-    path, _ := filepath.Abs(dir)
+    path, err := filepath.Abs(dir)
+    if err != nil {
+        t.Errorf("could not read directory %s", dir)
+    }
     paths, err := ioutil.ReadDir(path)
     if err != nil {
         t.Errorf("Could not list dir: %s, %v", dir, err)
     }
 
     for _, file := range paths {
-        fileExt := strings.ToLower(filepath.Ext(file.Name()))
-        ext := []string{"json", "csv", "zip"}
-        if ! file.IsDir() && contains(ext, fileExt) {
+        if !file.IsDir() && contains(ext, strings.ToLower(filepath.Ext(file.Name()))) {
             files = append(files, file.Name())
         }
     }
