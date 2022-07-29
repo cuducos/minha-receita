1cce4192a8befc3127567f581e9938aece2800f4
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-07-29 17:44:25 -0400

    Removes unlogged option
    This allows to easily fork the db on ElephantSQL

--- db/postgres/create.sql
+++ db/postgres/create.sql
@@ -1,4 +1,4 @@
-CREATE UNLOGGED TABLE IF NOT EXISTS {{ .TableFullName }} (
+CREATE TABLE IF NOT EXISTS {{ .TableFullName }} (
     {{ .IDFieldName }}       bigint NOT NULL PRIMARY KEY,
     {{ .JSONFieldName }}     jsonb NOT NULL
 );
