deb3e7f71c26c152d1c55ac0d1cffed4ad4a981a
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-07-30 10:50:51 -0400

    Revert "Removes unlogged option"
    Without this option the disk space used is too high (failed in BD with 500GB
    SSD).
    
    This reverts commit 1cce4192a8befc3127567f581e9938aece2800f4.

--- db/postgres/create.sql
+++ db/postgres/create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS {{ .TableFullName }} (
+CREATE UNLOGGED TABLE IF NOT EXISTS {{ .TableFullName }} (
     {{ .IDFieldName }}       bigint NOT NULL PRIMARY KEY,
     {{ .JSONFieldName }}     jsonb NOT NULL
 );
