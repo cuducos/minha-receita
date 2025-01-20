a38899f2d1af563c3f23c105546a5809a79e0e67
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2025-01-20 09:43:19 -0500

    Removes `--step-one` and `--step-two` from `transform`
    This feature introduced some bugs hard to debug, it worth it to just
    re-run the whole `transform` in case of errors.

--- transform/transform.go
+++ transform/transform.go
@@ -34,34 +34,6 @@ type kvStorage interface {
 	close() error
 }
 
-type mode int
-
-const (
-	// runs the two steps and cleans up temporary key-value directory
-	// from step one
-	both mode = iota
-
-	// loads relational data to a key-value store and does not delete it from
-	// the temporary directory
-	stepOne
-
-	// expects a path to the directory created in StepOne and use it to persist
-	// data to PostgreSQL
-	stepTwo
-)
-
-func transformMode(s1 bool, s2 string) (mode, error) {
-	switch {
-	case s1 && s2 != "":
-		return both, errors.New("cannot use both --step-one and --step-two")
-	case s1 && s2 == "":
-		return stepOne, nil
-	case !s1 && s2 != "":
-		return stepTwo, nil
-	}
-	return both, nil
-}
-
 func saveUpdatedAt(db database, dir string) error {
 	log.Output(1, "Saving the updated at date to the database…")
 	p := filepath.Join(dir, download.FederalRevenueUpdatedAt)
@@ -73,7 +45,7 @@ func saveUpdatedAt(db database, dir string) error {
 	return db.MetaSave("updated-at", string(v))
 }
 
-func runStepOne(dir string, pth string, l lookups) error {
+func createKeyValueStorage(dir string, pth string, l lookups) error {
 	kv, err := newBadgerStorage(pth)
 	if err != nil {
 		return fmt.Errorf("could not create badger storage: %w", err)
@@ -85,7 +57,7 @@ func runStepOne(dir string, pth string, l lookups) error {
 	return nil
 }
 
-func runStepTwo(dir string, pth string, db database, l lookups, maxParallelDBQueries, batchSize int, privacy bool) error {
+func createJSONs(dir string, pth string, db database, l lookups, maxParallelDBQueries, batchSize int, privacy bool) error {
 	kv, err := newBadgerStorage(pth)
 	if err != nil {
 		return fmt.Errorf("could not create badger storage: %w", err)
@@ -103,17 +75,8 @@ func runStepTwo(dir string, pth string, db database, l lookups, maxParallelDBQue
 
 // Transform the downloaded files for company venues creating a database record
 // per CNPJ
-func Transform(dir string, db database, max, s int, p, s1 bool, s2 string) error {
-	m, err := transformMode(s1, s2)
-	if err != nil {
-		return fmt.Errorf("error determining transform mode: %w", err)
-	}
-	var pth string
-	if m == stepTwo {
-		pth = s2
-	} else {
-		pth, err = os.MkdirTemp("", fmt.Sprintf("minha-receita-%s-*", time.Now().Format("20060102150405")))
-	}
+func Transform(dir string, db database, max, s int, p bool) error {
+	pth, err := os.MkdirTemp("", fmt.Sprintf("minha-receita-%s-*", time.Now().Format("20060102150405")))
 	if err != nil {
 		return fmt.Errorf("error creating temporary key-value storage: %w", err)
 	}
@@ -122,19 +85,8 @@ func Transform(dir string, db database, max, s int, p, s1 bool, s2 string) error
 	if err != nil {
 		return fmt.Errorf("error creating look up tables from %s: %w", dir, err)
 	}
-	switch m {
-	case stepOne:
-		if err := runStepOne(dir, pth, l); err != nil {
-			return err
-		}
-		fmt.Println(pth)
-	case stepTwo:
-		return runStepTwo(dir, pth, db, l, max, s, p)
-	case both:
-		if err := runStepOne(dir, pth, l); err != nil {
-			return err
-		}
-		return runStepTwo(dir, pth, db, l, max, s, p)
+	if err := createKeyValueStorage(dir, pth, l); err != nil {
+		return err
 	}
-	return nil
+	return createJSONs(dir, pth, db, l, max, s, p)
 }
