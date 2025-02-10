911f73f78050580276536617d3cfae71593939cc
Mauricio Nunes <mutila@gmail.com>
2025-02-10 12:35:38 -0300

    Update db/mongodb_test.go
    Co-authored-by: Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>

--- db/mongodb_test.go
+++ db/mongodb_test.go
@@ -9,9 +9,9 @@ func TestMongoDB(t *testing.T) {
 	id := "33683111000280"
 	json := `{"qsa": [{"name": 42}, {"name": "forty-two"}], "answer": 42}`
 
-	u := os.Getenv("TEST_DATABASE_URL")
+	u := os.Getenv("TEST_MONGODB_URL")
 	if u == "" {
-		t.Errorf("expected a posgres uri at TEST_DATABASE_URL, found nothing")
+		t.Errorf("expected a mongodb uri at TEST_MONGODB_URL, found nothing")
 		return
 	}
 	db, err := NewMongoDB("minhareceita")
