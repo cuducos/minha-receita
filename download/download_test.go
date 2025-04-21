0beb50ccdf77bb27bc196e0832e858ce6a053e8b
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2025-04-21 16:24:32 -0400

    Adds function with all the expected JSON fields
    This supports #279 by:
    - offering a way to validate the indexes passed as input to the
      extra-indexes command
    - making array assertions available to other packages than download

--- download/download_test.go
+++ download/download_test.go
@@ -67,35 +67,3 @@ func httpTestServer(t *testing.T, cs []string) *httptest.Server {
 			http.ServeFile(w, r, path.Join("..", "testdata", cs[idx]))
 		}))
 }
-
-func assertArraysHaveSameItems(t *testing.T, a1, a2 []string) {
-	if len(a1) != len(a2) {
-		t.Errorf("arrays lengths are different: %d != %d", len(a1), len(a2))
-		return
-	}
-
-	c1 := make(map[string]int)
-	c2 := make(map[string]int)
-	for _, v := range a1 {
-		c1[v]++
-	}
-	for _, v := range a2 {
-		c2[v]++
-	}
-
-	diff := make(map[string]struct{})
-	for k := range c1 {
-		if c1[k] != c2[k] {
-			diff[k] = struct{}{}
-		}
-	}
-	for k := range c2 {
-		if c1[k] != c2[k] {
-			diff[k] = struct{}{}
-		}
-	}
-
-	for k := range diff {
-		t.Errorf("%q appears %d in the first array, but %d in the second array", k, c1[k], c2[k])
-	}
-}
