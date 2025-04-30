ffa62cbe9c0b88d751c8e56ce0134944e41787d4
Mauricio Nunes <mutila@gmail.com>
2025-04-30 16:58:14 -0300

    correcoes de vars

--- db/mongodb.go
+++ db/mongodb.go
@@ -235,8 +235,6 @@ func (m *MongoDB) ExtraIndexes(idxs []string) error {
 	c := m.db.Collection(companyTableName)
 	var i []mongo.IndexModel
 	for _, v := range idxs {
-		k := fmt.Sprintf("json.%s", v)
-		n := fmt.Sprintf("idx_json.%s", v)
 		i = append(i, mongo.IndexModel{
 			Keys:    bson.D{{Key: fmt.Sprintf("json.%s", v), Value: 1}},
 			Options: options.Index().SetName(fmt.Sprintf("idx_json.%s", v)),
