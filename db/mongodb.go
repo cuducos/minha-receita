053cd73309b3aee7b098d83d78116cd39f625f6d
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2025-07-18 13:33:54 -0400

    Implements CNAE search for MongoDB
    Co-authored-by: Mauricio Nunes <mutila@gmail.com>

--- db/mongodb.go
+++ db/mongodb.go
@@ -248,7 +248,19 @@ func (m *MongoDB) Search(q *Query) (string, error) {
 			f["json.cnae_fiscal"] = bson.M{"$in": q.UF}
 		}
 	}
-	// TODO: figure out how to search for CNAE & secondary CNAE
+	if len(q.CNAE) > 0 {
+		if len(q.CNAE) == 1 {
+			f["$or"] = []bson.M{
+				{"cnae_fiscal": q.CNAE[0]},
+				{"cnaes_secundarios.codigo": bson.M{"$in": q.CNAE}},
+			}
+		} else {
+			f["$or"] = []bson.M{
+				{"cnae_fiscal": bson.M{"$in": q.CNAE}},
+				{"cnaes_secundarios.codigo": bson.M{"$in": q.CNAE}},
+			}
+		}
+	}
 	if q.Cursor != nil {
 		f["json.cnpj"] = bson.M{"$gt": *q.Cursor}
 	}
