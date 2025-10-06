0e023779efcb2e870b2563f953d977de586bb192
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2025-10-06 16:44:31 -0400

    Deprecates checksum commands
    The official source never implemented them and I have seen no usage out
    there for this part of the tooling (happy to revert if that is not the
    case).

--- cmd/check.go
+++ cmd/check.go
@@ -6,45 +6,7 @@ import (
 	"github.com/cuducos/minha-receita/check"
 )
 
-var (
-	checksumSrcDir string
-	deleteZipFiles bool
-)
-
-const checksumHelper = `
-Checksum of the downloaded files.
-
-Even though the official website of the Brazilian Federal Revenue does not offer
-a checksum for their files, this command can be used to create or check the checksum
-of downloaded files.`
-
-var rootChecksumCmd = &cobra.Command{
-	Use:   "checksum",
-	Short: "Checksum of the downloaded files.",
-	Long:  checksumHelper,
-}
-
-var createChecksumCmd = &cobra.Command{
-	Use:   "create",
-	Short: "Creates checksum of downloaded files.",
-	RunE: func(_ *cobra.Command, _ []string) error {
-		if err := assertDirExists(); err != nil {
-			return err
-		}
-		return check.CreateChecksum(dir)
-	},
-}
-
-var checkChecksumCmd = &cobra.Command{
-	Use:   "check",
-	Short: "Checks checksum of downloaded files.",
-	RunE: func(_ *cobra.Command, _ []string) error {
-		if err := assertDirExists(); err != nil {
-			return err
-		}
-		return check.CheckChecksum(checksumSrcDir, dir)
-	},
-}
+var deleteZipFiles bool
 
 const checkHelper = `
 Checks the integrity of the downloaded ZIP files.
@@ -66,23 +28,8 @@ var checkCmd = &cobra.Command{
 }
 
 func checkCLI() *cobra.Command {
-	for _, cmd := range []*cobra.Command{createChecksumCmd, checkChecksumCmd} {
-		cmd = addDataDir(cmd)
-
-		rootChecksumCmd.AddCommand(cmd)
-	}
-
-	checkChecksumCmd.Flags().StringVarP(
-		&checksumSrcDir,
-		"src-directory",
-		"s",
-		defaultDataDir,
-		"directory of the checksum file(s) to compare with",
-	)
-
 	checkCmd = addDataDir(checkCmd)
 	checkCmd.Flags().BoolVarP(&deleteZipFiles, "delete", "x", deleteZipFiles, "deletes ZIP files that fails the check")
-	checkCmd.AddCommand(rootChecksumCmd)
 
 	return checkCmd
 }
